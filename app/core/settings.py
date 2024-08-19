4dbb302fbb28fbe64f2607810736903e8a26d095
sir-heed <sademolaadedeji@gmail.com>
2024-08-19 11:52:30 +0100

    updated allowed hosts

--- app/core/settings.py
+++ app/core/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = get_required('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
